973a942acfce1e98c99b6257ea1304f780afb62b
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2021-12-22 15:41:00 +0100

    Update install-mautic.sh

--- install-mautic.sh
+++ install-mautic.sh
@@ -13,20 +13,20 @@ sudo apt update
 sudo apt upgrade
 
 # Install NGINX
-sudo apt install nginx
+sudo apt install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo ufw allow http
 sudo chown www-data:www-data /usr/share/nginx/html -R
 
 #Install MariaDB
-sudo apt install mariadb-server mariadb-client
+sudo apt install -y mariadb-server mariadb-client
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 sudo mysql_secure_installation
 
 #Install PHP7.4
-sudo apt install php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl
+sudo apt install -y php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl
 sudo systemctl start php7.4-fpm
 sudo systemctl enable php7.4-fpm
 
@@ -55,7 +55,7 @@ sed -i 's/DBPASSWORD/'$MAUTICPWD'/g' mautic.sql
 mysql -u root < mautic.sql
 
 # Install required and recommended PHP Modules
-sudo apt install php-imagick php7.4-fpm php7.4-mysql php7.4-common php7.4-gd php7.4-imap php7.4-imap php7.4-json php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-gmp
+sudo apt install -y php-imagick php7.4-fpm php7.4-mysql php7.4-common php7.4-gd php7.4-imap php7.4-imap php7.4-json php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-gmp
 cp mautic.conf.template mautic.conf
 sed -i 's/YOURSERVERNAME/'$YOURSERVERNAME'/g' mautic.conf
 cp mautic.conf /etc/nginx/conf.d/
@@ -66,10 +66,10 @@ sudo systemctl reload nginx
 sudo systemctl reload php7.4-fpm
 
 # Enable HTTPS
-sudo apt install certbot
+sudo apt install -y certbot
 sudo mkdir -p /var/www/mautic/.well-known/acme-challenge
 sudo chown www-data:www-data /var/www/mautic/.well-known/acme-challenge
-sudo apt install python3-certbot-nginx
+sudo apt install -y python3-certbot-nginx
 sudo certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email $YOUREMAIL -d $YOURSERVERNAME
 sudo sed sed -i 's/443 ssl/443 ssl http2/g' /etc/nginx/conf.d/mautic.conf
 sudo systemctl reload nginx
